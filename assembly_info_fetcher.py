1e9b94a370b875090662ea9e7faf3eaea106fbec
owen151128 <owen151128@gmail.com>
2025-09-03 23:11:28 +0900

    feat(stream): Add fetch streaming info

--- assembly_info_fetcher.py
+++ assembly_info_fetcher.py
@@ -7,6 +7,7 @@ import requests
 
 from urllib.parse import urlparse, urlencode, parse_qs
 
+from streaming_info import StreamingInfo
 from video_info import VideoInfo
 from video_item import VideoItem
 
@@ -23,14 +24,9 @@ class AssemblyInfoFetcher:
         self.params = parsed.params
         # parse_qs 결과가 key str, value list 이므로, key str, value str 로 변환
         self.queries = {k: v[0] for k, v in parse_qs(parsed.query).items()}
-        self._init_const_queries()
         self.requests_headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
         }
-
-    def _init_const_queries(self):
-        self.queries["cmd"] = "movieInfo"
-        self.queries["no"] = ""
 
     def _to_query_string(self, additional_params: dict = None, exclude_params: list = None) -> str:
         if additional_params is None:
@@ -63,11 +59,39 @@ class AssemblyInfoFetcher:
         return assembly_video_info
 
     def fetch_assembly_video_info(self) -> VideoInfo:
-        query_string = self._to_query_string({"vv": f"{self._get_timestamp()}"}, ["menu"])
+        additional_params = {
+            "cmd": "movieInfo",
+            "no": "",
+            "vv": f"{self._get_timestamp()}",
+        }
+        query_string = self._to_query_string(additional_params, ["menu"])
         fetch_url = f"{self.base_url}{Consts.INFO_FETCH_PATH}?{query_string}&"
         result = requests.get(fetch_url, headers=self.requests_headers)
-        if result.status_code != 200:
-            raise ConnectionError(f"Error fetching assembly video info: {result.status_code}/{result.text}")
+        result.raise_for_status()
         assembly_video_info = self._parse_assembly_info_response(result.json())
 
         return assembly_video_info
+
+    # number 는 no 에 해당하는 인자로 index 값으로 추정
+    # wv 는 wv 에 해당하는 인자
+    def fetch_assembly_streaming_info(self, mc, ct1, ct2, ct3, number, wv) -> StreamingInfo:
+        query = {
+            "cmd": "fileInfo",
+            "mc": mc,
+            "ct1": ct1,
+            "ct2": ct2,
+            "ct3": ct3,
+            "no": number,
+            "wv": wv,
+            "xreferer": "",
+            "vv": f"{self._get_timestamp()}",
+        }
+        query_string = urllib.parse.urlencode(query)
+        fetch_url = f"{self.base_url}{Consts.INFO_FETCH_PATH}?{query_string}&"
+        result = requests.get(fetch_url, headers=self.requests_headers)
+        result.raise_for_status()
+        stream_list = result.json()["filePath"]
+        default_stream_key = stream_list.get("default") or ""
+        stream_list.pop("default")
+
+        return StreamingInfo(default_stream_key, stream_list)
---
+++ streaming_info.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class StreamingInfo:
+    def __init__(self, default_stream_key, stream_list):
+        self.default_stream_key = default_stream_key
+        self.stream_list = stream_list
