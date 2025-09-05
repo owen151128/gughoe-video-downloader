c922c25f6d19154e03e7c3c163d6dc601781e548
owen151128 <owen151128@gmail.com>
2025-09-05 18:32:27 +0900

    feat(work): Add background worker

---
+++ background_worker.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+class BackgroundWorker(QThread):
+    result_ready = pyqtSignal(object)
+    error_occurred = pyqtSignal(str)
+
+    def __init__(self, func, *args, **kwargs):
+        super().__init__()
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        try:
+            result = self.func(*self.args, **self.kwargs)
+            self.result_ready.emit(result)
+        except BaseException as e:
+            self.error_occurred.emit(str(e))
